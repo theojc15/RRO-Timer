555f9b64f71cb1f572994b794444c20e5013a691
theojc15 <79826207+theojc15@users.noreply.github.com>
2022-10-12 16:41:33 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace RRO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(494, 740);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseCompatibleTextRendering = true;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(14, 38);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(500, 102);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(524, 14);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(286, 150);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(820, 14);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(5);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox5.Location = new System.Drawing.Point(12, 775);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(248, 158);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 199.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 308);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1238, 377);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(602, 740);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 52);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "START";
+            this.button2.UseCompatibleTextRendering = true;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(494, 798);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(318, 52);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "START";
+            this.button3.UseCompatibleTextRendering = true;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button4.AutoSize = true;
+            this.button4.Location = new System.Drawing.Point(710, 740);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(102, 52);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "START";
+            this.button4.UseCompatibleTextRendering = true;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1262, 981);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RRO Timer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace RRO
+{
+    public partial class Form1 : Form
+    {
+        int min, sec;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            min = 0;
+            sec = 0;
+            button1.Text = "90 Menit";
+            button2.Text = "60 Menit";
+            button4.Text = "30 Menit";
+            button3.Text = "START";
+            textBox1.ForeColor = Color.Black;
+            textBox1.Text = min.ToString("D2") + " : " + sec.ToString("D2");
+            pictureBox2.Image = new Bitmap(RRO.Properties.Resources.LOGO_RK_VECTOR__HI_RES__Preview);
+            pictureBox3.Image = new Bitmap(RRO.Properties.Resources.RRO_Logo__1_);
+            pictureBox4.Image = new Bitmap(RRO.Properties.Resources.Promo_Awal_Events__IG____RRO_22_1a);
+            pictureBox5.Image = new Bitmap(RRO.Properties.Resources.logo_its_biru_transparan);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (sec == 0)
+            {
+                min = min - 1;
+                sec = 60;
+                sec = sec - 1;
+            }
+            else
+            {
+                sec = sec - 1;
+            }
+
+            if (min == 0 && sec <= 0)
+            {
+                timer1.Enabled = false;
+                button3.Text = "START";
+            }
+
+            if (min < 5)
+            {
+                if(textBox1.ForeColor == Color.Red)
+                {
+                    textBox1.ForeColor = Color.Black;
+                }
+                else if (textBox1.ForeColor == Color.Black)
+                {
+                    textBox1.ForeColor = Color.Red;
+                }
+            }
+            textBox1.Text = min.ToString("D2") + " : " + sec.ToString("D2");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            min = 90;
+            sec = 0;
+            textBox1.Text = min.ToString("D2") + " : " + sec.ToString("D2");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            min = 60;
+            sec = 0;
+            textBox1.Text = min.ToString("D2") + " : " + sec.ToString("D2");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            min = 30;
+            sec = 0;
+            textBox1.Text = min.ToString("D2") + " : " + sec.ToString("D2");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (min != 0 && button3.Text == "START")
+            {
+                timer1.Enabled = true;
+                button3.Text = "STOP";
+            }
+            else if (button3.Text == "STOP")
+            {
+                timer1.Enabled = false;
+                button3.Text = "START";
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            textBox1.Location = new Point((this.Width / 2) - (textBox1.Width / 2), (this.Height / 2) - (textBox1.Height / 2));
+        }
+    }
+}
